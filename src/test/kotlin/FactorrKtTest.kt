16d306958a76d467c569e5e7952d713b3a1cbc3d
kedkeke <ked44839@hotmail.com>
2020-01-17 13:15:23 +0700

    Merge branch 'master' of E:\we with conflicts.

--- src/test/kotlin/FactorrKtTest.kt
+++ src/test/kotlin/FactorrKtTest.kt
@@ -6,7 +6,7 @@ class FactorrKtTest {
 
     @Test
     fun factor() {
-      val num: Int = factor(1,1)
-      assertEquals(2,num)
+      // var num: Int = factor(1,1)
+      //  assertEquals(2,num)
     }
 }
